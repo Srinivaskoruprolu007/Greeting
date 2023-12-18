868531322e2acaf0907f9db69f9291974767046c
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2023-12-18 13:45:30 +0530

    some temp styles

--- script.js
+++ script.js
@@ -34,18 +34,40 @@ $(document).ready(function () {
 
   //   event listener for random play button click
   var audioFiles = [
-    "./Assets/Audio/01_nuvve_leni_nenu.mp3",
-    "./Assets/Audio/02_Baitikochi Chuste.mp3",
-    "./Assets/Audio/03_Adento_gani.mp3",
-    "./Assets/Audio/04_Meghalu_lekunna.mp3",
-    "./Assets/Audio/05_Sapta Sagaralu Dhaati.mp3",
+    {
+      src: "./Assets/Audio/01_nuvve_leni_nenu.mp3",
+      image: "./Assets/Images/Nuvve leni nenu.jpeg",
+      title: "Nuvve leni nenu",
+    },
+    {
+      src: "./Assets/Audio/02_Baitikochi Chuste.mp3",
+      image: "./Assets/Images/Baitikocchi chusthey.jpeg",
+      title: "Baitikocchi chusthey",
+    },
+    {
+      src: "./Assets/Audio/03_Adento_gani.mp3",
+      image: "./Assets/Images/Adhento_gani.jpeg",
+      title: "Adento gani",
+    },
+    {
+      src: "./Assets/Audio/04_Meghalu_lekunna.mp3",
+      image: "./Assets/Images/meghalu_lekunna.jpeg",
+      title: "Meghalu lekunna",
+    },
+    {
+      src: "./Assets/Audio/05_Sapta Sagaralu Dhaati.mp3",
+      image: "./Assets/Images/Saptha_sagaralu.jpg",
+      title: "Sapta sagaralu",
+    },
   ];
   $("#playRandom").on("click", function () {
     var randomIndex = Math.floor(Math.random() * audioFiles.length);
     console.log(randomIndex);
     var randomAudio = audioFiles[randomIndex];
 
-    $("#audioPlayer").attr("src", randomAudio);
+    $("#audioPlayer").attr("src", randomAudio.src);
+    $("#songImage").attr("src", randomAudio.image);
+    $("#songTitle").text(randomAudio.title);
 
     document.getElementById("audioPlayer").play();
   });
